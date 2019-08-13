d4c792c055e2e3d5bbfe3e5111905cdb6142a983
drzhang <654014730@qq.com>
2019-08-13 23:49:52 +0800

    etcd watch

--- prepare/etcd_usage/demo6/demo.go
+++ prepare/etcd_usage/demo6/demo.go
@@ -4,16 +4,22 @@ import (
 	"context"
 	"fmt"
 	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/mvcc/mvccpb"
 	"time"
 )
 
 func main() {
 	var (
-		config  clientv3.Config
-		client  *clientv3.Client
-		kv      clientv3.KV
-		err     error
-		getResp *clientv3.GetResponse
+		config             clientv3.Config
+		client             *clientv3.Client
+		kv                 clientv3.KV
+		err                error
+		getResp            *clientv3.GetResponse
+		watchStartRevision int64
+		watcher            clientv3.Watcher
+		watchChan          clientv3.WatchChan
+		watchResp          clientv3.WatchResponse
+		event              *clientv3.Event
 	)
 
 	config = clientv3.Config{
@@ -46,4 +52,25 @@ func main() {
 		fmt.Println("value:", string(getResp.Kvs[0].Value))
 	}
 
+	//当前etcd集群事务id revision，单调递增的
+	watchStartRevision = getResp.Header.Revision + 1
+	fmt.Println("从该版本向后监听：", watchStartRevision)
+
+	//创建一个watcher
+	watcher = clientv3.NewWatcher(client)
+
+	//启动watcher监听,clientv3.WithRev从哪个版本开始监听
+	watchChan = watcher.Watch(context.TODO(), "/cron/lock/job2", clientv3.WithRev(watchStartRevision))
+
+	//处理kv变化事件
+	for watchResp = range watchChan {
+		for _, event = range watchResp.Events {
+			switch event.Type {
+			case mvccpb.PUT:
+				fmt.Println("修改为：", string(event.Kv.Value), "Revision:", event.Kv.Version, event.Kv.ModRevision)
+			case mvccpb.DELETE:
+				fmt.Println("删除了，Revision:", event.Kv.ModRevision)
+			}
+		}
+	}
 }
