6e47c3403503512596ce0351d4b640fbbfb84cd8
drzhang <654014730@qq.com>
2019-09-18 21:46:31 +0800

    添加初始化服务方法

--- koala/registry/plugin_mgr.go
+++ koala/registry/plugin_mgr.go
@@ -1,6 +1,7 @@
 package registry
 
 import (
+	"context"
 	"fmt"
 	"sync"
 )
@@ -26,4 +27,29 @@ func (p *PluginMgr) registerPlugin(plugin Registry) (err error) {
 	}
 	p.plugins[plugin.Name()] = plugin
 	return
+}
+
+// initRegistry 初始化注册服务
+func (p *PluginMgr) initRegistry(ctx context.Context, name string, opts ...Option) (registry Registry, err error) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	plugin, ok := p.plugins[name]
+	if !ok {
+		err = fmt.Errorf("plugin %s not exists", name)
+		return
+	}
+	registry = plugin
+	err = plugin.Init(ctx, opts...)
+	return
+}
+
+// RegisterPlugin 注册插件
+func RegisterPlugin(registry Registry) (err error) {
+	return pluginMgr.registerPlugin(registry)
+}
+
+// InitRegistry 初始化注册中心
+func InitRegistry(ctx context.Context, name string, opts ...Option) (registry Registry, err error) {
+	return pluginMgr.initRegistry(ctx, name, opts...)
 }
--- koala/registry/service.go
+++ koala/registry/service.go
@@ -1,5 +1,13 @@
 package registry
 
 type Service struct {
+	Name  string  `json:"name"`
+	Nodes []*Node `json:"nodes"`
+}
 
+type Node struct {
+	Id     string `json:"id"`
+	Ip     string `json:"ip"`
+	Port   int    `json:"port"`
+	Weight int    `json:"weight"`
 }
