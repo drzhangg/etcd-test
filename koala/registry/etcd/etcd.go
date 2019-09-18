fde8b3fc16cdfe56bb2e5f58d3c40a229c2d04eb
drzhang <654014730@qq.com>
2019-09-19 00:09:00 +0800

    add etcd

---
+++ koala/registry/etcd/etcd.go
@@ -0,0 +1,71 @@
+package etcd
+
+import (
+	"context"
+	"fmt"
+	"github.com/drzhangg/etcd-test/koala/registry"
+	"go.etcd.io/etcd/clientv3"
+	"path"
+)
+
+type EtcdRegistry struct {
+	options   *registry.Options
+	client    *clientv3.Client
+	serviceCh chan *registry.Service
+}
+
+var (
+	etcdRegistry *EtcdRegistry = &EtcdRegistry{
+		serviceCh: make(chan *registry.Service, 8),
+	}
+)
+
+func init() {
+	registry.RegisterPlugin(etcdRegistry)
+	go etcdRegistry.run()
+}
+
+// Name 插件名字
+func (e *EtcdRegistry) Name() string {
+	return "etcd"
+}
+
+func (e *EtcdRegistry) Init(ctx context.Context, opts ...registry.Options) (err error) {
+	e.options = &registry.Options{}
+	for _, opt := range opts {
+		opt(e.options)
+	}
+
+	e.client, err = clientv3.New(clientv3.Config{
+		Endpoints:   e.options.Address,
+		DialTimeout: e.options.Timeout,
+	})
+	if err != nil {
+		return
+	}
+	return
+}
+
+// Register 服务注册
+func (e *EtcdRegistry) Register(ctx context.Context, service *registry.Service) (err error) {
+	select {
+	case e.serviceCh <- service:
+	default:
+		err = fmt.Errorf("register chan is full
+		return
+	}
+	return
+}
+
+// Unregister 反注册
+func (e *EtcdRegistry) Unregister(ctx context.Context, service *registry.Service) (err error) {
+	return
+}
+
+func (e *EtcdRegistry) run() {
+
+}
+
+func (e *EtcdRegistry) servicePath(service *registry.Service) string {
+	return path.Join(e.options.RegistryPath, service.Name)
+}
--- koala/registry/options.go
+++ koala/registry/options.go
@@ -3,8 +3,10 @@ package registry
 import "time"
 
 type Options struct {
-	Address []string //注册中心地址
-	Timeout time.Duration
+	Address      []string //注册中心地址
+	Timeout      time.Duration
+	RegistryPath string //注册路径
+	HeartBeat    int64  //心跳时间
 }
 
 type Option func(opts *Options)
