d89c80978a4b4ec2cd93e5b156e5e0e5c7a7698a
drzhang <654014730@qq.com>
2019-09-20 08:14:48 +0800

    etcd register

--- koala/registry/etcd/etcd.go
+++ koala/registry/etcd/etcd.go
@@ -2,21 +2,39 @@ package etcd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/drzhangg/etcd-test/koala/registry"
 	"go.etcd.io/etcd/clientv3"
 	"path"
+	"time"
+)
+
+const (
+	MaxServiceNum          = 8
+	MaxSyncServiceInterval = time.Second * 10
 )
 
 type EtcdRegistry struct {
 	options   *registry.Options
 	client    *clientv3.Client
 	serviceCh chan *registry.Service
+
+	registryServiceMap map[string]*RegisterService
+}
+
+type RegisterService struct {
+	id      clientv3.LeaseID
+	service *registry.Service
+
+	registered  bool
+	keepAliveCh <-chan *clientv3.LeaseKeepAliveResponse
 }
 
 var (
 	etcdRegistry *EtcdRegistry = &EtcdRegistry{
-		serviceCh: make(chan *registry.Service, 8),
+		serviceCh:          make(chan *registry.Service, 8),
+		registryServiceMap: make(map[string]*RegisterService, MaxServiceNum),
 	}
 )
 
@@ -64,6 +82,96 @@ func (e *EtcdRegistry) Unregister(ctx context.Context, service *registry.Service
 
 func (e *EtcdRegistry) run() {
 
+	//ticker := time.NewTicker(MaxSyncServiceInterval)
+	for {
+		select {
+		case service := <-e.serviceCh:
+			_, ok := e.registryServiceMap[service.Name]
+			if ok {
+				break
+			}
+			registryService := &RegisterService{
+				service: service,
+			}
+			e.registryServiceMap[service.Name] = registryService
+		default:
+			e.registerOrKeepAlive()
+			time.Sleep(time.Millisecond * 500)
+		}
+	}
+}
+
+// registerOrKeepAlive 注册或者续租
+func (e *EtcdRegistry) registerOrKeepAlive() {
+	for _, registryService := range e.registryServiceMap {
+		if registryService.registered {
+			e.keepAlive(registryService)
+			continue
+		}
+
+		e.registerService(registryService)
+	}
+}
+
+// keepAlive 续租
+func (e *EtcdRegistry) keepAlive(registryService *RegisterService) {
+	select {
+	case resp := <-registryService.keepAliveCh:
+		if resp == nil {
+			registryService.registered = false
+			return
+		}
+		fmt.Printf("services:%s node:%s ttl:%v\n", registryService.service.Name, registryService.service.Nodes[0].Ip, registryService.service.Nodes[0].Port)
+	}
+	return
+
+	return
+}
+
+// registerService 注册服务
+func (e *EtcdRegistry) registerService(registryService *RegisterService) (err error) {
+	resp, err := e.client.Grant(context.TODO(), e.options.HeartBeat)
+	if err != nil {
+		return
+	}
+
+	// 获取租约id
+	registryService.id = resp.ID
+	for _, node := range registryService.service.Nodes {
+		tmp := &registry.Service{
+			Name: registryService.service.Name,
+			Nodes: []*registry.Node{
+				node,
+			},
+		}
+
+		data, err := json.Marshal(tmp)
+		if err != nil {
+			continue
+		}
+
+		//获取节点路径
+		key := e.serviceNodePath(tmp)
+
+		_, err = e.client.Put(context.TODO(), key, string(data), clientv3.WithLease(resp.ID))
+		if err != nil {
+			continue
+		}
+
+		ch, err := e.client.KeepAlive(context.TODO(), resp.ID)
+		if err != nil {
+			continue
+		}
+		registryService.keepAliveCh = ch
+		registryService.registered = true
+	}
+	return
+}
+
+// serviceNodePath 获取节点路径
+func (e *EtcdRegistry) serviceNodePath(service *registry.Service) string {
+	nodeIP := fmt.Sprintf("%s:%d", service.Nodes[0].Ip, service.Nodes[0].Port)
+	return path.Join(e.options.RegistryPath, nodeIP)
 }
 
 func (e *EtcdRegistry) servicePath(service *registry.Service) string {
---
+++ prepare/crontab/master/Config.go
@@ -0,0 +1,10 @@
+package master
+
+// 程序配置
+type Config struct {
+	ApiPort         int      `json:"apiPort"`
+	ApiReadTimeout  int      `json:"apiReadTimeout"`
+	ApiWriteTimeout int      `json:"apiWriteTimeout"`
+	EtcdEndpoints   []string `json:"etcdEndpoints"`
+	WebRoot         string   `json:"webRoot"`
+}
