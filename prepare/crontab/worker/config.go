975513bef964b287d4ede9d35308c3022dd4deaa
drzhang <654014730@qq.com>
2019-10-12 00:18:25 +0800

    worker config

---
+++ prepare/crontab/worker/config.go
@@ -0,0 +1,38 @@
+package worker
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+//程序配置
+type Config struct {
+	EtcdEndpoints         []string `json:"etcdEndpoints"`
+	EtcdDialTimeout       int      `json:"etcdDialTimeout"`
+	MongodbUri            string   `json:"mongodbUri"`
+	MongodbConnectTimeout int      `json:"mongodbConnectTimeout"`
+	JobLogBatchSize       int      `json:"jobLogBatchSize"`
+	JobLogCommitTimeout   int      `json:"jobLogCommitTimeout"`
+}
+
+var (
+	G_config *Config
+)
+
+func InitConfig(filename string) (err error) {
+	var (
+		content []byte
+		conf    Config
+	)
+
+	//读取配置文件
+	if content, err = ioutil.ReadFile(filename); err != nil {
+		return
+	}
+
+	if err = json.Unmarshal(content, &conf); err != nil {
+		return
+	}
+	G_config = &conf
+	return
+}
