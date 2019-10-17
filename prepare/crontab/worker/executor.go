f06a9217def7bf7659ce525eeed61293b87de20c
drzhang <654014730@qq.com>
2019-10-17 17:40:59 +0800

    etcd test executor

--- prepare/crontab/worker/executor.go
+++ prepare/crontab/worker/executor.go
@@ -1,6 +1,10 @@
 package worker
 
-import "github.com/drzhangg/etcd-test/prepare/crontab/common"
+import (
+	"github.com/drzhangg/etcd-test/prepare/crontab/common"
+	"github.com/drzhangg/go-crontab/worker"
+	"os/exec"
+)
 
 // 任务执行器
 type Executor struct {
@@ -10,8 +14,31 @@ var (
 	G_executor *Executor
 )
 
+// 执行一个任务
 func (executor *Executor) ExecuteJob(info *common.JobExecuteInfo) {
+	go func() {
+		var (
+			cmd     *exec.Cmd
+			result  *common.JobExecuteResult
+			jobLock *JobLock
+		)
 
+		//任务结果
+		result = &common.JobExecuteResult{
+			ExecuteInfo: info,
+			Output:      make([]byte, 0),
+		}
+
+		//初始化分布式锁
+		jobLock = worker.G_jobMgr.CreateJobLock(info.Job.Name)
+
+		//记录任务开始时间
+
+		// 上锁
+		// 随机睡眠(0~1s)
+
+		//判断上锁是否成功
+	}()
 }
 
 // 初始化执行器
