489f843422c5f19c318dd46d461c6bae34c6c909
drzhang <654014730@qq.com>
2019-10-23 15:07:05 +0800

    etcd 保存任务

--- prepare/crontab/master/jobMgr.go
+++ prepare/crontab/master/jobMgr.go
@@ -1,3 +1,83 @@
 package master
 
+import (
+	"context"
+	"encoding/json"
+	"github.com/coreos/etcd/clientv3"
+	"github.com/drzhangg/etcd-test/prepare/crontab/common"
+	"time"
+)
 
+// 任务管理器，进行任务的crud
+type JobMgr struct {
+	client *clientv3.Client
+	kv     clientv3.KV
+	lease  clientv3.Lease
+}
+
+var (
+	G_jobMgr *JobMgr
+)
+
+//初始化管理器，进行一些etcd的初始化
+func InitJobMgr() (err error) {
+	var (
+		config clientv3.Config
+		client *clientv3.Client
+		kv     clientv3.KV
+		lease  clientv3.Lease
+	)
+	config = clientv3.Config{
+		Endpoints:   G_config.EtcdEndpoints,
+		DialTimeout: time.Duration(G_config.EtcdDialTimeout) * time.Millisecond,
+	}
+
+	//建立etcd连接
+	if client, err = clientv3.New(config); err != nil {
+		return
+	}
+
+	//得到kv和lease
+	kv = clientv3.NewKV(client)
+	lease = clientv3.NewLease(client)
+
+	//赋值单例
+	G_jobMgr = &JobMgr{
+		client: client,
+		kv:     kv,
+		lease:  lease,
+	}
+	return
+}
+
+// 保存任务
+func (jobMgr *JobMgr) SaveJob(job *common.Job) (oldJob *common.Job, err error) {
+	var (
+		jobKey    string
+		jobValue  []byte
+		putResp   *clientv3.PutResponse
+		oldJobObj common.Job
+	)
+
+	//etcd保存的key
+	jobKey = common.JOB_SAVE_DIR + job.Name
+
+	//任务信息
+	if jobValue, err = json.Marshal(job); err != nil {
+		return
+	}
+
+	//保存到etcd,withPrevKV()是为了获取操作前已经有的key-value
+	if putResp, err = jobMgr.kv.Put(context.TODO(), jobKey, string(jobValue), clientv3.WithPrevKV()); err != nil {
+		return
+	}
+	//如果当前是更新操作，那么返回旧的值
+	if putResp.PrevKv != nil { //putResp.PrevKv获取put前的值
+		if err = json.Unmarshal(putResp.PrevKv.Value, &oldJobObj); err != nil {
+			err = nil
+			return
+		}
+		oldJob = &oldJobObj
+	}
+	return
+}
