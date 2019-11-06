a7e796bcf8d63e314a26379ab977ed072a5f751c
drzhang <654014730@qq.com>
2019-11-06 21:56:56 +0800

    etcd job log

--- coreos/etcd/common/protocol.go
+++ coreos/etcd/common/protocol.go
@@ -2,6 +2,23 @@ package common
 
 import "strings"
 
+//任务执行日志
+type JobLog struct {
+	JobName      string `json:"jobName" bson:"jobName"`           //任务执行名称
+	Command      string `json:"command" bson:"command"`           //脚本执行命令
+	Err          string `json:"err" bson:"err"`                   //err
+	Output       string `json:"output" bson:"output"`             //输出脚本
+	PlanTime     int64  `json:"planTime" bson:"planTime"`         //计划开始时间
+	ScheduleTime int64  `json:"scheduleTime" bson:"scheduleTime"` //实际调度时间
+	StartTime    int64  `json:"startTime" bson:"startTime"`       //任务开始执行时间
+	EndTime      int64  `json:"endTime" bson:"endTime"`           //任务结束执行时间
+}
+
+//日志排序规则	1：正排，-1：倒排
+type SortLogByStartTime struct {
+	SortOrder int `bson:"startTime"`
+}
+
 // 任务日志过滤条件
 type JobFilter struct {
 	JobName string `bson:"jobName"`
--- coreos/etcd/master/service/log.go
+++ coreos/etcd/master/service/log.go
@@ -5,6 +5,7 @@ import (
 	"github.com/drzhangg/etcd-test/coreos/etcd/common"
 	"github.com/mongodb/mongo-go-driver/mongo"
 	"github.com/mongodb/mongo-go-driver/mongo/clientopt"
+	"github.com/mongodb/mongo-go-driver/mongo/findopt"
 	"time"
 )
 
@@ -37,16 +38,33 @@ func InitLog() (err error) {
 // name 过滤条件
 // skip	跳过指定数量的数据
 // limit	读取指定数量的数据
-func (log *Log) LogList(name string, skip int, limit int) (logArr []Log, err error) {
+func (log *Log) LogList(name string, skip int, limit int) (logArr []*common.JobLog, err error) {
 	var (
 		jobFilter *common.JobFilter
+		logSort   *common.SortLogByStartTime
+		cursor    mongo.Cursor
+		jobLog    *common.JobLog
 	)
 	//初始化数组
-	logArr = make([]Log, 0)
+	logArr = make([]*common.JobLog, 0)
 	//查找过滤条件
 	jobFilter = &common.JobFilter{JobName: name}
 
+	//按照任务开始时间倒排
+	logSort = &common.SortLogByStartTime{SortOrder: -1}
 
+	//按照条件过滤查询
+	if cursor, err = log.logCollection.Find(context.TODO(), jobFilter, findopt.Sort(logSort), findopt.Skip(int64(skip)), findopt.Limit(int64(limit))); err != nil {
+		return
+	}
+	defer cursor.Close(context.TODO())
 
+	for cursor.Next(context.TODO()) {
+		jobLog = &common.JobLog{}
+		if err = cursor.Decode(jobLog); err != nil {
+			continue
+		}
+		logArr = append(logArr, jobLog)
+	}
 	return
 }
